e5bc8ae515112495fa4f043f0a32c09146043e6d
yulianaromaniv <108033373+yulianaromaniv@users.noreply.github.com>
2022-07-11 12:56:57 +0300

    Add files via upload

---
+++ Hw4(main).cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[6];
+            for (int i=0; i<6; i++)
+            {
+                people[i] = Person.Input();
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                people[i].Output();
+            }
+            Console.WriteLine("-------------------------------------");
+            for (int i = 0; i < 6; i++)
+            {
+                if (people[i].Age(2022) <= 16)
+                {
+                    people[i].ChangeName("Very yung");
+                     
+                }
+                people[i].Output();
+            }
+            Console.WriteLine("-------------------------------------");
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = i + 1; j <6; j++)
+                {
+                    if (people[i] == people[j])
+                    {
+                        Console.WriteLine($"{people[i]} and {people[j]} have the same names");
+                    }
+                    else
+                    { Console.WriteLine($"Person's names are not equals"); }
+                }
+            }
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW4
+{
+    class Person
+    {
+        private string name;
+        private int birthYear;
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int BirthYear
+        {
+            set { birthYear = value; }
+            get { return birthYear; }
+        }
+        public Person()
+        {
+            name = "ivan";
+            birthYear = 2000;
+        }
+        public Person(string _name, int _birthYear)
+        {
+            name = _name;
+            birthYear = _birthYear;
+        }
+        public int Age(int nowYear)
+        {
+            int age = nowYear - birthYear;
+            return age;
+        }
+        public static Person Input()
+        {
+            Console.WriteLine("input the name of person ");
+            string name = Console.ReadLine();
+            Console.WriteLine("input the birthday year of person: ");
+            int birthYear =  Convert.ToInt32(Console.ReadLine());
+            Person person = new Person(name , birthYear );
+            return person;
+        }
+        public void ChangeName(string _Name)
+        {
+            name = _Name;
+        }
+        public override string ToString()
+        {
+            return "person's name " + name + "person's age " + Age(2022);
+        }
+        public void Output()
+        {
+            Console.WriteLine(" Person name: " + name + " ; person age: " + Age(2022));
+        }
+        public static bool operator ==(Person first, Person second)
+        {
+            return (first.name == second.name);
+        }
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first.name == second.name);
+        }
+    }
+}
